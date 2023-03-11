56802061445313f462d1b5fffaa2cc8f94aeaaff
2190031 <2190031@ipisa.edu.do>
2023-03-11 11:07:34 -0400

    modificacion de login

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template
-from db_models import db, role, user, file
+from flask import Flask, render_template, request, redirect
+from db_models import db, role, user, file, historial, version, error, licence, comment, preference, paytransaction, migration
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:c55h32o5n4Mg@localhost/c_cloud'
-db.init_app(app)
+# db.init_app(app)
 
 @app.route('/')
 def index():
     return render_template('index.html') 
 
-@app.route('/login')
+@app.route('/sign_up')
 def form():
-    return render_template('login.html')
+    return render_template('sign_up.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
