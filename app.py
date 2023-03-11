80f03c2252999a67f5b57ccffdd0e45a2033fb21
2190031 <2190031@ipisa.edu.do>
2023-03-10 21:28:30 -0400

    Modelos de la base de datos, archivo de requerimientos actualizado, plantilla de login hecha

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from db_models import db, role, user, file
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:c55h32o5n4Mg@localhost/c_cloud'
+db.init_app(app)
 
 @app.route('/')
 def index():
     return render_template('index.html') 
+
+@app.route('/login')
+def form():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
