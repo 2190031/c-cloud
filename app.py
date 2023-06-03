1c27a075b4ff421604204cc82c0706511a4a6794
Brayan <brayanmartinez07149@gmail.com>
2023-06-03 16:55:04 -0400

    paypal botonoes y funciones

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, redirect, session
-
+from flask import Flask, render_template, jsonify, request
 from db_models import db, defaultroles
 from render_pages import *
 from file_management import load_file, create_file, load_file__blank
 from user_session import login, signup
+import paypalrestsdk
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:c55h32o5n4Mg@localhost/c_cloud'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Mauricio2021@localhost/c_cloud'
 app.config['SECRET_KEY'] = "secret_key"
 
 db.init_app(app)
@@ -22,6 +23,124 @@ with app.app_context():
 @app.route('/')
 def index():
     return render_index()
+paypalrestsdk.configure({
+    "mode": "sandbox",  # sandbox or live
+    "client_id": "AdvcMAZRch_a2_IX8HcOahjLOrd0PGTh4sjj_Fq6UdgrVUsf38Gwttt04IVoCR5hc0nKcONkEGXjtghB",
+    "client_secret": "EM2-afNHVKHKLSoFtoDKbQ4Ap1U3VTmaXiXT3-8CwOILetphM5qt_KwEy8244739kZSDGJddmbcGZ5ar"
+})
+
+@app.route('/planes')
+def planes():
+    return render_template('planes.html')
+
+@app.route('/payment', methods=['POST'])
+def payment():
+    payment = paypalrestsdk.Payment({
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "http://127.0.0.1:5000/execute",
+            "cancel_url": "http://127.0.0.1:5000/"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "Plan Básico",
+                    "sku": "BASICO",
+                    "price": "10.00",
+                    "currency": "USD",
+                    "quantity": 1
+                }]
+            },
+            "amount": {
+                "total": "10.00",
+                "currency": "USD"
+            },
+            "description": "This is the payment transaction description."
+        }]
+    })
+
+    if payment.create():
+        print('Payment success!')
+    else:
+        print(payment.error)
+
+    return jsonify({'paymentID': payment.id})
+
+
+@app.route('/execute', methods=['POST'])
+def execute():
+    success = False
+
+    payment = paypalrestsdk.Payment.find(request.form['paymentID'])
+
+    if payment.execute({'payer_id': request.form['payerID']}):
+        print('Execute success!')
+        success = True
+    else:
+        print(payment.error)
+
+    return jsonify({'success': success})
+
+
+@app.route('/payment/premium', methods=['POST'])
+def payment_premium():
+    payment = paypalrestsdk.Payment({
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "http://127.0.0.1:5000/execute/premium",
+            "cancel_url": "http://127.0.0.1:5000/"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "Plan Premium",
+                    "sku": "PREMIUM",
+                    "price": "20.00",
+                    "currency": "USD",
+                    "quantity": 1
+                }]
+            },
+            "amount": {
+                "total": "20.00",
+                "currency": "USD"
+            },
+            "description": "This is the payment transaction description."
+        }]
+    })
+
+    if payment.create():
+        print('Payment success!')
+    else:
+        print(payment.error)
+
+    return jsonify({'paymentID': payment.id})
+
+
+@app.route('/execute/premium', methods=['POST'])
+def execute_premium():
+    success = False
+
+    payment = paypalrestsdk.Payment.find(request.form['paymentID'])
+
+    if payment.execute({'payer_id': request.form['payerID']}):
+        print('Execute success!')
+        success = True
+    else:
+        print(payment.error)
+
+    return jsonify({'success': success})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
     
 @app.route('/signup', methods=['POST', 'GET'])
 def sign_up():
@@ -79,6 +198,8 @@ def notfound():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('notfound.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
